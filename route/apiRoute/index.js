1fb058ecf6cddfa5a8d9ce23d39ce334aea8546d
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-12-19 19:52:22 -0500

    finalized delete functionality so that it now deletes on the front end

--- route/apiRoute/index.js
+++ route/apiRoute/index.js
@@ -4,7 +4,7 @@ const { v4: uuidv4} = require('uuid');
 const fs = require('fs');
 
 // create link to database file 
-const { notes } = require('../../db/db.json');
+let { notes } = require('../../db/db.json');
 
 // view notes 
 router.get('/notes', (req,res) => {    
@@ -35,6 +35,8 @@ router.delete('/notes/:id', (req, res) => {
     let data = notes;
 
     const newData = data.filter(note => note.id.toString() !== noteId);
+    
+    notes = newData
 
     fs.writeFileSync('./db/db.json', JSON.stringify({notes: newData}),null,2);
 
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // set route 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3003;
 
 //parse incoming data 
 app.use(express.urlencoded({extended: true}));
